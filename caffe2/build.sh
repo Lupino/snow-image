85e22d43f5991fa7b591fdd9dfbb47c73962b37d
Lupino <lmjubuntu@gmail.com>
2018-02-19 20:40:42 +0800

    update caffe2 src path

--- caffe2/build.sh
+++ caffe2/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/caffe2/caffe2.git
+git clone https://github.com/caffe2/caffe2.git caffe2-src
 
 cd caffe2
 git submodules update --init
@@ -9,7 +9,7 @@ cd ..
 mkdir build
 cd build
 
-cmake ../caffe2 -DBLAS=OpenBLAS \
+cmake ../caffe2-src -DBLAS=OpenBLAS \
          -DUSE_OPENCV=off \
          -DPYTHON_EXECUTABLE=/usr/local/bin/python3 \
          -DUSE_MPI=OFF \
